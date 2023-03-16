b3a8fff85d31fe487e17cadeb285fedefc89b398
ercode1337 <eric.melin@outlook.com>
2023-03-16 21:09:24 +0200

    exercise ok

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahDeg) {
+  let celsius = (fahDeg - 32) * 5/9;
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celDeg) {
+  let fahrenheit = (celDeg * 9/5 + 32);
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
