c3e2e6b2b76eacc48218af6aae0d2c3f61d1859a
ercode1337 <eric.melin@outlook.com>
2023-03-17 15:11:27 +0200

    exercise ok

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = ""; // stores new reversed string
+    for (let i = string.length - 1; i >= 0; i--) { // loop
+        newString += string[i]; 
+    }
+    return newString; // returns the reversed string
 };
 
 // Do not edit below this line
