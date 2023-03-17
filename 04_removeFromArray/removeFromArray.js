b0c0e5ac466151fba79db70fa81ce2accd694b57
ercode1337 <eric.melin@outlook.com>
2023-03-17 15:31:47 +0200

    exercise ok

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...args) {
+    array = array.filter(item => !args.includes(item)) // removes (args) from array
+    return array; // returns array without (args)
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
