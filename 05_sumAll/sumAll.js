2cc13e28b24146f9a067866325514fe84d88e87e
ercode1337 <eric.melin@outlook.com>
2023-03-17 23:06:27 +0200

    exercise ok

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if (numOne < 0 || numTwo < 0) {
+        return "ERROR";
+    }
+    if (numOne > numTwo) {
+        let all = 1;
 
-};
+        for (let i = numTwo; i < numOne; i++) {
+            all += (i + 1)
+        }
+        return all;
+    }
+    if (Number.isInteger(numOne) && Number.isInteger(numTwo)) {
+        let all = 1;
+    
+        for (let i = numOne; i < numTwo; i++){
+            all += (i + 1) 
+        }
+    
+        return all;
+    } 
+    else {
+        return 'ERROR'
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
