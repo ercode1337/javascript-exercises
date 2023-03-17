1b83d25e1143dd1f8d126e498b3ddf1b4007f664
ercode1337 <eric.melin@outlook.com>
2023-03-17 13:39:22 +0200

    exercise ok - functions coded

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,59 @@
-const add = function() {
-	
+const add = function(oneSum, secSum) {
+	let result = oneSum + secSum;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(oneSum, secSum) {
+	let result = oneSum - secSum;
+  return result;
 };
 
-const sum = function() {
-	
+
+const sum = function(arr) {
+  if(arr.length == 0){
+    return 0;
+  }
+  else if(arr.length == 1){
+    return arr[0];
+  }
+  else{
+    let sum = 0;
+    for(let i=0; i < arr.length; i++){
+      sum += arr[i];
+    }
+    return sum;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if(arr.length == 0){
+    return 0;
+  }
+  else if(arr.length == 1){
+    return arr[0];
+  }
+  else{
+    let mult = 1;
+    for(let i=0; i < arr.length; i++){ // loop start
+      mult *= arr[i];
+    }
+    return mult;
+  }
 };
 
-const power = function() {
-	
+const power = function(oneSum, secSum) {
+  let result = Math.pow(oneSum, secSum);
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+
+  for (let i = 2; i <= number; i++) {
+    result = result * i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
