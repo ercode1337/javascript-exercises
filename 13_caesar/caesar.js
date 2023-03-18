bffd06aa59843847d2c5faf23d707e37dc913793
ercode1337 <eric.melin@outlook.com>
2023-03-18 16:19:23 +0200

    exercise ok

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,26 @@
-const caesar = function() {
-
-};
+const caesar = function(str, shift) { // encrypt by using caesar
+    let encodedStr = '';
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    
+    for (let char of str) {
+        let index = alphabet.indexOf(char.toLowerCase());
+        // account for negative shifts
+        if (shift < 0) shift = alphabet.length + shift;
+        // handle non-letters
+        if (index === -1) {
+            encodedStr += char;
+            continue;
+        }
+        // calculate new index
+        index = index + (shift % alphabet.length);
+        // wrap around
+        if (index >= alphabet.length) index = index - alphabet.length;
+        // add letter to encoded string
+        encodedStr += (char === char.toUpperCase()) ? alphabet[index].toUpperCase() : alphabet[index];
+    }
+    
+    return encodedStr;
+}
 
 // Do not edit below this line
-module.exports = caesar;
+module.exports = caesar
