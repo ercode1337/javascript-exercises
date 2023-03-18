179eda07c906ea8654ed9782d2f913279f0d9943
ercode1337 <eric.melin@outlook.com>
2023-03-18 15:47:37 +0200

    exercise ok

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldestPerson = people.reduce((oldestAge, person) => {
+        const age = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : new Date().getFullYear() - person.yearOfBirth;
+        if (age > oldestAge.age) { // checks if the age is higher
+            return {
+                name: person.name,
+                age
+            }
+        } else {
+            return oldestAge;
+        }
+    }, { age: 0 });
+    return oldestPerson;
 };
 
 // Do not edit below this line
