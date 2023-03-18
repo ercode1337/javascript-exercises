c34635ee50528f7ce73ba3fad2a79be9ac91ed87
ercode1337 <eric.melin@outlook.com>
2023-03-18 12:46:28 +0200

    exercise ok

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (typeof num === 'string') {
+        num = parseInt(num);
+    }
+    if (Number.isInteger(num) && num >= 0) {
+    let i;
+    let fib = [0, 1];
+
+    for (i = 2; i <= num; i++) {
+        fib[i] = fib[i - 2] + fib[i - 1];
+    }
+        return fib[num];
+    }
+    else {
+        return 'OOPS';
+    }
 
 };
 
