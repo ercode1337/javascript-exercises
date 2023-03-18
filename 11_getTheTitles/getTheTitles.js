c8ddf154bd104cd23868b15b695a9d47f082b9aa
ercode1337 <eric.melin@outlook.com>
2023-03-18 13:05:36 +0200

    exercise ok

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles = books.map(({title}) => title); // gets the titles
+    return titles;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
